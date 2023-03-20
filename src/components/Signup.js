8be145cbb598961e9154fd4be154ab6f0a91948c
Melvin Mathew <vinumon2000@gmail.com>
2023-03-20 21:33:23 +0530

    signup updated

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -20,25 +20,26 @@ export default function Signup(){
 
     return(
         <div>
-            <div className="position-absolute top-50 start-50 translate-middle">
-                <form  onSubmit={signup}>
-                    <div className="mb-3">
+            <div className="card  mx-auto" style={{width:370,backgroundColor:'wheat'}}>
+                <form  className="card-body" onSubmit={signup}>
+                    <h5 className="card-header">Signup Form</h5><br></br>
+                    <div className="card-text mb-3">
                         <label className="form-label">User Name</label>
                         <input className="form-control" type="text" value={username} onChange={(event)=>setUsername(event.target.value)}></input>
                     </div>
-                    <div className="mb-3">
+                    <div className="card-text mb-3">
                         <label className="form-label">Email</label>
                         <input type="email" className="form-control" value={mail} onChange={(event)=>setMail(event.target.value)}></input>
                     </div>
-                    <div className="mb-3">
+                    <div className="card-text mb-3">
                         <label className="form-label">Password</label>
                         <input type="password" value={password} onChange={(event)=>setPassword(event.target.value)} className="form-control"></input>
                     </div>
-                    <div className="mb-3">
+                    <div className="card-text mb-3">
                         <label className="form-label">re-enter password</label>
                         <input type="password" value={retype} onChange={(event)=>setRetype(event.target.value)} className="form-control"></input>
                     </div>
-                    <div className="mb-3">
+                    <div className="card-text mb-3">
                         <button type="submit" style={{margin:10}} className="btn btn-success">Submit</button>
                         <button type="reset" onClick={reset} className="btn btn-info">Reset</button>
                     </div>
